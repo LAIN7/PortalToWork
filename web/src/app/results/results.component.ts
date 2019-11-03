5241900c73f60222fe034a3885377590d377a8d0
LAIN7 <LAIN7GIT@gmail.com>
2019-11-02 22:54:20 -0500

    calculating distance - BP DS

--- web/src/app/results/results.component.ts
+++ web/src/app/results/results.component.ts
@@ -13,27 +13,65 @@ export class ResultsComponent implements OnInit {
   results: Job[] = [];
   loading = false;
   origin: any;
+  mapOrigin: any;
+
   constructor(private jobService: JobsService, private locationService: LocationService) { }
 
-  byJobsWithLocations(job: Job): boolean {
-    return job.locations.data.length > 0 && !!job.locations.data[0].lat && !!job.locations.data[0].lng;
+  radians(degrees: number) {
+    return degrees * Math.PI / 180;   
+  }
+
+  getDistance = (location: Location) => {
+    let lat1 = +location.lat;
+    let lon1 = +location.lng;
+
+    let lat2 = this.origin.lat;
+    let lon2 = this.origin.lng;
+    var R = 6371e3; // metres
+    var φ1 = this.radians(lat1);
+    var φ2 = this.radians(lat2);
+    var Δφ = this.radians(lat2-lat1);
+    var Δλ = this.radians(lon2-lon1);
+    
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    
+    var d = R * c;
+    
+    return d * 0.000621371;
+  }
+
+  byJobsWithLocations = (job: Job) => {
+    const firstLocation = job.locations.data[0];
+    if(firstLocation && firstLocation.lat && firstLocation.lng) {
+      const distance = this.getDistance(firstLocation);
+      if(distance > 1.0) {
+        return false;
+      }
+  
+      return true; 
+    }
+
+    return false;    
   }
   ngOnInit() {
-    this.locationService.getLocation((position) => {
-      this.origin = { "lat": position.coords.latitude, "lng": position.coords.longitude };
-    });
     this.loading = true;
-    this.jobService.getJobs().subscribe((jobs) => {
-      this.results = jobs.data.filter(this.byJobsWithLocations);
+    this.jobService.getJobs().subscribe((jobs) => {        
+      this.locationService.getLocation((position) => {
+        this.mapOrigin = this.origin = { "lat": position.coords.latitude, "lng": position.coords.longitude };
+        this.results = jobs.data.filter(this.byJobsWithLocations);
+      });
       this.loading = false;
     });
   }
 
   locateMapHere(location: Location) {
-    this.origin = undefined;
+    this.mapOrigin = undefined;
 
     setTimeout(() => {
-      this.origin = { lat: +location.lat, lng: +location.lng };
+      this.mapOrigin = { lat: +location.lat, lng: +location.lng };
     }, 1)
   }
 
